8923281b5ee99828fa3f2c1f554abf26d4281a0e
nikotlichnik <shtreisnikita@gmail.com>
2018-09-19 19:54:55 +0300

    Добавлена защита от XSS

--- templates/index.php
+++ templates/index.php
@@ -17,11 +17,11 @@
         <?php foreach($lots as $key => $item): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$item['img_path']; ?>" width="350" height="260" alt="<?=$item['name']; ?>">
+                    <img src="<?=$item['img_path']; ?>" width="350" height="260" alt="<?=htmlspecialchars($item['name']); ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$item['category']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['name']; ?></a></h3>
+                    <span class="lot__category"><?=htmlspecialchars($item['category']); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($item['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>

ae5f0d782e154b2111067d0e22a1f3164b66c68b
nikotlichnik <shtreisnikita@gmail.com>
2018-09-20 19:48:23 +0300

    Изменение логики работы функции таймера

--- functions.php
+++ functions.php
@@ -40,28 +40,11 @@ function format_price($price) {
  * Возвращает значение таймера для лота
  * @return string
  */
-//function get_timer() {
-//    $seconds_in_hour = 60 * 60;
-//    $seconds_in_minute = 60;
-//
-//    $now_stamp = time();
-//    $end_stamp = strtotime('tomorrow');
-//    $diff_stamp = $end_stamp - $now_stamp;
-//    $hours = floor($diff_stamp / $seconds_in_hour);
-//    $minutes = floor($diff_stamp % $seconds_in_hour / $seconds_in_minute);
-//
-//    $hours = $hours < 10 ? '0' . $hours : $hours;
-//    $minutes = $minutes < 10 ? '0' . $minutes : $minutes;
-//    $timer = $hours . ':' . $minutes;
-//
-//    return $timer." ".$now_stamp;
-//}
-
 function get_timer() {
-    $now_stamp = time();
-    $end_stamp = strtotime('tomorrow');
-    $diff_stamp = $end_stamp - $now_stamp;
-    $timer = date('H:i', $diff_stamp);
+    $dateNow = new DateTime('now');
+    $dateTomorrow = new DateTime('tomorrow');
+    $datesDiff = $dateTomorrow->diff($dateNow);
+    $timer = $datesDiff->format('%H:%I');
 
     return $timer;
 }

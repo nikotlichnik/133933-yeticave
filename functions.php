fe561a0ee668f9d07b4af94a2c71d73d607c69bf
nikotlichnik <shtreisnikita@gmail.com>
2018-09-29 11:06:12 +0300

    Подключение к базе данных вынесено в отдельную функцию

--- functions.php
+++ functions.php
@@ -1,4 +1,57 @@
 <?php
+
+/**
+ * Возвращает соединение к базе данных
+ * @return mysqli
+ */
+function connect_db() {
+    $host = '127.0.0.1';
+    $user = 'root';
+    $password = '';
+    $database = 'yeticave';
+
+    $con = mysqli_connect($host, $user, $password, $database);
+    mysqli_set_charset($con, 'utf-8');
+
+    return $con;
+}
+
+/**
+ * Получает из БД ассоциативный массив с категориями для меню
+ * @param $con
+ * @return array|null
+ */
+function get_categories($con) {
+    $sql = 'SELECT name FROM categories';
+    $result = mysqli_query($con, $sql);
+
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+/**
+ * Получает из БД ассоциативный массив со списком лотов
+ * @param $con
+ * @return array|null
+ */
+function get_lots($con) {
+    $sql = 'SELECT
+              l.name,
+              l.start_price,
+              l.img_path,
+              MAX(b.bet)   AS current_price,
+              COUNT(b.bet) AS bet_counter,
+              c.name       AS category
+            FROM lots l
+              JOIN categories c ON l.category = c.id
+              LEFT JOIN bets b ON l.id = b.lot
+            WHERE l.expiration_date > now()
+            GROUP BY l.id
+            ORDER BY l.creation_date DESC';
+    $result = mysqli_query($con, $sql);
+
+    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
 /**
  * Функция-шаблонизатор
  * @param string $name
--- index.php
+++ index.php
@@ -3,35 +3,13 @@ date_default_timezone_set("Europe/Moscow");
 
 require_once 'functions.php';
 
-$host = '127.0.0.1';
-$user = 'root';
-$password = '';
-$database = 'yeticave';
-
-$con = mysqli_connect($host, $user, $password, $database);
-mysqli_set_charset($con, 'utf-8');
+$con = connect_db();
 
 // Запрос лотов
-$sql = 'SELECT
-  l.name,
-  l.start_price,
-  l.img_path,
-  MAX(b.bet)   AS current_price,
-  COUNT(b.bet) AS bet_counter,
-  c.name       AS category
-FROM lots l
-  JOIN categories c ON l.category = c.id
-  LEFT JOIN bets b ON l.id = b.lot
-WHERE l.expiration_date > now()
-GROUP BY l.id
-ORDER BY l.creation_date DESC';
-$result = mysqli_query($con, $sql);
-$lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$lots = get_lots($con);
 
 // Запрос категорий для футера
-$sql = 'SELECT name FROM categories';
-$result = mysqli_query($con, $sql);
-$categories_footer = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$categories_footer = get_categories($con);
 
 $title = 'YetiCave - Главная страница.';
 

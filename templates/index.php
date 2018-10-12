a87adedfabba7b2879fba2aa1a4b87a86d71295e
nikotlichnik <shtreisnikita@gmail.com>
2018-10-13 00:16:23 +0300

    Рефакторинг index.php

--- templates/index.php
+++ templates/index.php
@@ -2,9 +2,9 @@
     <h2 class="promo__title">Нужен стафф для катки?</h2>
     <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
     <ul class="promo__list">
-        <?php foreach ($categories as $key => $value): ?>
+        <?php foreach ($categories as $category): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="#"><?=$value; ?></a>
+                <a class="promo__link" href="#"><?=$category['name']; ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
